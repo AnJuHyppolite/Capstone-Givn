a8d2abe8e0b3537fd9123deb73a78ded7019ecf9
Shoaib Dar <SDAR30@gmail.com>
2021-08-29 12:02:17 -0400

    basic schema added

--- back-end/db/schema.sql
+++ back-end/db/schema.sql
@@ -1,6 +1,24 @@
-DROP TABLE IF EXISTS test;
+DROP DATABASE IF EXISTS givn_dev;
 
-CREATE TABLE test (
+CREATE DATABASE givn_dev;
+
+CREATE TABLE users (
     id SERIAL PRIMARY KEY, 
-    name TEXT
+    email TEXT NOT NULL,
+    display_name VARCHAR(20),
+    address VARCHAR(100)
 );
+
+CREATE TABLE posts (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(40) NOT NULL,
+    description TEXT NOT NULL,
+    category TEXT NOT NULL,
+    location TEXT NOT NULL,
+    user_id INT REFERENCES users (id) ON DELETE CASCADE
+);
+
+CREATE TABLE photos (
+    photo TEXT NOT NULL,
+    post_id INT REFERENCES posts (id) ON DELETE CASCADE
+)
