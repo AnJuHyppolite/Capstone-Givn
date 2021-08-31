a715e5bab5e9c311ab4e83f1f1a4e5807c820f62
Shoaib Dar <SDAR30@gmail.com>
2021-08-30 20:55:18 -0400

    added seed and schema files

--- back-end/db/schema.sql
+++ back-end/db/schema.sql
@@ -2,23 +2,46 @@ DROP DATABASE IF EXISTS givn_dev;
 
 CREATE DATABASE givn_dev;
 
+\c givn_dev;
+
 CREATE TABLE users (
     id SERIAL PRIMARY KEY, 
-    email TEXT NOT NULL,
-    display_name VARCHAR(20),
+    email VARCHAR(50) NOT NULL UNIQUE,
+    display_name VARCHAR(30),
     address VARCHAR(100)
 );
 
-CREATE TABLE posts (
+CREATE TABLE items (
     id SERIAL PRIMARY KEY,
-    title VARCHAR(40) NOT NULL,
+    title VARCHAR(50) NOT NULL,
     description TEXT NOT NULL,
-    category TEXT NOT NULL,
     location TEXT NOT NULL,
-    user_id INT REFERENCES users (id) ON DELETE CASCADE
+    created_at TEXT NOT NULL,
+    status TEXT NOT NULL,
+    is_biodegradable BOOLEAN NOT NULL,
+    expiration INT DEFAULT 0,
+    giver_id INT REFERENCES users (id) ON DELETE CASCADE
+);
+
+CREATE TABLE transactions (
+    id SERIAL PRIMARY KEY,
+    time TEXT NOT NULL,
+    getter_id INT REFERENCES users (id),
+    giver_id INT REFERENCES users (id),
+    item_id INT REFERENCES items (id)
+);
+
+CREATE TABLE categories (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+CREATE TABLE category_items (
+    id SERIAL PRIMARY KEY,
+    category_id INT REFERENCES categories (id),
+    item_id INT REFERENCES items (id)
 );
 
 CREATE TABLE photos (
-    photo TEXT NOT NULL,
-    post_id INT REFERENCES posts (id) ON DELETE CASCADE
-)
+    photo_url VARCHAR(50) NOT NULL,
+    item_id INT REFERENCES items (id) ON DELETE CASCADE
+);
--- back-end/db/seed.sql
+++ back-end/db/seed.sql
@@ -1,8 +1,24 @@
-INSERT INTO test (name) VALUES
-('Monday'),
-('Tuesday'),
-('Wednesday'),
-('Thursday'),
-('Friday'),
-('Saturday'),
-('Sunday');
+\c givn_dev;
+
+INSERT INTO users(email, display_name, address) VALUES
+('testemail1@gmail.com', 'initial tester', 'New York'),
+('masterjeff@yahoo.com' , 'master Jeff', 'queens'),
+('heyyall@gmail.com', 'heeeeey', 'Bronx'),
+('johndoe15@hotmail.com', 'johnnyboy', 'Jersey City, NJ');
+
+INSERT INTO items(title, description, location, created_at, status, is_biodegradable, expiration, giver_id) VALUES 
+('wine rack', '6-bottle holder', 'New York', '8/19/21 6PM', 'active', false, 0, 1),
+('bar stools', 'wooden', 'Bronx', '8:19 p.m.', 'active', false, 0, 3),
+('nutella', 'unopened', 'Jersey City', '11:34 a.m', 'active', true, 22,2);
+
+INSERT INTO transactions(time, getter_id, giver_id, item_id) VALUES 
+('8:06 pm', 1, 2, 2), ('2:06 am', 2, 1, 1), ('8:06 pm', 1, 2, 1);
+
+INSERT INTO categories(name) VALUES 
+('food'),('clothes'),('electronics'),('books'),('hardware'),('shoes'),('furniture');
+
+INSERT INTO category_items(category_id, item_id) VALUES 
+(6,1), (4,2), (1,3);
+
+INSERT INTO photos(photo_url, item_id) VALUES 
+('https://dummyimage.com/300.png/09f/fff', 2), ('https://dummyimage.com/300.png/09f/aa1', 2), ('https://dummyimage.com/300.png/aaf/fff', 1);
