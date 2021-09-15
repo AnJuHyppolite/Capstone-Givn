d0030ec28b64ec46dfc6bf1a0337f6d52d26b062
Jeffrey <jeffreysebastian@pursuit.org>
2021-09-14 20:57:49 -0400

    complete show page

--- front-end/src/Components/Item.js
+++ front-end/src/Components/Item.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const Item = ({ item }) => {
   return (
     <li>
@@ -6,11 +8,13 @@ const Item = ({ item }) => {
           {item.created_at} ({item.location})
         </h5>
       </div>
-      <h1>{item.title}</h1>
-      <img
-        src="https://cb.scene7.com/is/image/Crate/11BottleWineRackBlckGrphtSHF17/$web_pdp_main_carousel_high$/190411134606/11-bottle-graphite-wine-rack.jpg"
-        alt="item"
-      />
+      <Link to={`/posts/${item.id}`}>
+        <h1>{item.title}</h1>
+        <img
+          src="https://cb.scene7.com/is/image/Crate/11BottleWineRackBlckGrphtSHF17/$web_pdp_main_carousel_high$/190411134606/11-bottle-graphite-wine-rack.jpg"
+          alt="item"
+        />
+      </Link>
       <p>Expiring In: {item.expiration} (day(s))</p>
       <p>Recycling is good!</p>
       <div>
