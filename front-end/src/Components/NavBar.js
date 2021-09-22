009b85e356e32878416ae6b260adce6393294a35
Jordan Bobadilla <jordanboba08@gmail.com>
2021-09-21 21:01:54 -0400

    add dropdown update

--- front-end/src/Components/NavBar.js
+++ front-end/src/Components/NavBar.js
@@ -2,13 +2,14 @@ import { NavLink } from "react-router-dom";
 import "../Styles/NavBar.css";
 import { signOut } from "../Services/Firebase";
 import { UserContext } from "../Providers/UserProvider";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import logo from "../Assets/onlinelogomaker-091421-1958-0090.png";
 
 const NavBar = () => {
   const user = useContext(UserContext);
   const history = useHistory();
+  const [open, setOpen] = useState(false);
 
   useEffect(() => {
     if (!user) {
@@ -18,6 +19,11 @@ const NavBar = () => {
 
   const handleSignOut = () => {
     signOut();
+    setOpen(false);
+  };
+
+  const handleDropdown = () => {
+    setOpen((prevOpen) => !prevOpen);
   };
 
   return (
@@ -37,17 +43,25 @@ const NavBar = () => {
             <NavLink exact to="/posts/new">
               Give
             </NavLink>
-            <NavLink exact to="/profile">
-              Profile
-            </NavLink>
-            <img src={user.photoURL} />
-            {/* <ul>
-            <li></li>
-            <li>
-              <button onClick={handleSignOut}>Sign out</button>
-            </li>
-          </ul> */}
           </div>
+          <img
+            src={user.photoURL}
+            alt="user-photo"
+            className="profile"
+            onClick={handleDropdown}
+          />
+          <ul className={!open ? "dropdown" : null} id="user-menu">
+            <li className="one">
+              <i class="fas fa-user-circle"></i>
+              <NavLink exact to="/profile">
+                Profile
+              </NavLink>
+            </li>
+            <li>
+              <i class="fas fa-sign-out-alt"></i>
+              <button onClick={handleSignOut}>Sign Out</button>
+            </li>
+          </ul>
         </nav>
       ) : (
         <nav>
@@ -65,18 +79,18 @@ const NavBar = () => {
               Give
             </NavLink>
           </div>
-          <ul>
+          <ul className={!open ? "dropdown" : null}>
             <li className="one">
               <i class="fas fa-sign-in-alt"></i>
               <NavLink to={"/login"}>Log In</NavLink>
             </li>
 
             <li>
-            <i class="fas fa-sign-out-alt"></i>
+              <i class="fas fa-user-plus"></i>
               <NavLink to={"/signup"}>Sign Up</NavLink>
             </li>
           </ul>
-          <button className="left-icon">
+          <button className="left-icon" onClick={handleDropdown}>
             <i class="fas fa-caret-down"></i>
           </button>
         </nav>
