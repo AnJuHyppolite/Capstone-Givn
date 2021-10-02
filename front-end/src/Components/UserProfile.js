0c73106cad22f4ef3153aa3d9368f77a54209e13
Jeffrey <jeffreysebastian@pursuit.org>
2021-10-02 15:40:02 -0400

    created userprofile and user item

--- front-end/src/Components/UserProfile.js
+++ front-end/src/Components/UserProfile.js
@@ -3,55 +3,56 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router";
 import axios from "axios";
 
-const UserProfile = () => {
-  const API = apiURL();
-  const { id } = useParams();
-  const [userProfile, setUserProfile] = useState({});
-  const [photos, setPhotos] = useState([]);
-
-  useEffect(() => {
-    const fetchPhoto = async () => {
-      let res = await axios.get(`${API}/items/${id}/photos`);
-      debugger;
-      setPhotos(res.data);
-    };
-    const fecthUser = async () => {
-      let res = await axios.get(`${API}/users/${id}`);
-      // debugger;
-      // console.log(res.data);
-      setUserProfile(res.data);
-    };
-    fecthUser();
-    fetchPhoto();
-  }, [API, id]);
-
-  // useEffect(() => {
-  //   const fetchPhoto = async () => {
-  //     let res = await axios.get(`${API}/items/${id}/photos`);
-  //     debugger;
-  //     setPhotos(res.data);
-  //   };
-  //   fetchPhoto();
-  // }, [API, id]);
-
-  const { email, score, display_name, photo_url, address } = userProfile;
-  return (
-    <div>
-      <div>
+  const UserProfile = () => {
+    const API = apiURL();
+    const { id } = useParams();
+    const [userProfile, setUserProfile] = useState({});
+    const [activeItems, setActiveItems] = useState([]);
+    const [inactiveItems, setInactiveItems] = useState([]);
+  
+    useEffect(() => {
+      const fetchPhoto = async () => {
+        let res = await axios.get(`${API}/users/${id}/items`);
+        setActiveItems(res.data.filter((item) => item.status === "active"));
+        setInactiveItems(res.data.filter((item) => item.status === "inactive"));
+      };
+      const fecthUser = async () => {
+        let res = await axios.get(`${API}/users/${id}`);
+        setUserProfile(res.data);
+      };
+      fecthUser();
+      fetchPhoto();
+    }, [API, id]);
+  
+    const { email, score, display_name, photo_url, address } = userProfile;
+    return (
+      <section className="Profile">
         <div>
-          <img src={photo_url} alt={display_name} />
-          <h1>{display_name}</h1>
-          <h4>{email}</h4>
-          <h4>{score}</h4>
-          <p>{address}</p>
+          <section>
+            <img src={photo_url} alt={display_name} />
+            <h2>{display_name}</h2>
+            <h3>{email}</h3>
+            <div>
+              <h3>
+                {address} | Score(s): {score}
+              </h3>
+            </div>
+          </section>
+          Given Items
+          <div className="profile-items-list">
+            <div className="inactive-items">
+              {inactiveItems.map((item) => (
+                <UserItems item={item} key={item} />
+              ))}
+            </div>
+            <div className="active-items">
+              {activeItems.map((item) => (
+                <UserItems item={item} key={item} />
+              ))}
+            </div>
+          </div>
         </div>
-        Given Items
-        {/* {photos.map((photo) => {
-          return <img src={photo.photo_url} alt="imageItem" />;
-        })} */}
-      </div>
-    </div>
-  );
-};
-
+      </section>
+    );
+  };
 export default UserProfile;
