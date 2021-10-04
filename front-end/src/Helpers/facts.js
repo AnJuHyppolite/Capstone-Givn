abdb9760b35953b673ef1efed83196e9fff6792e
anjuhypp <anjuhypp@gmail.com>
2021-10-04 19:26:22 -0400

    get branch back to pre-randonmization attempts

--- front-end/src/Helpers/facts.js
+++ front-end/src/Helpers/facts.js
@@ -1,98 +1,80 @@
 const facts = [
-  // {
-  //   category: "Electronics",
-  //   facts: "Recycling the waste from electronics saves space in the landfills and prevents environmental pollution caused by the toxins in the electronics.",
-  //   source:
-  //     "https://www.techdump.org/3-amazing-benefits-electronics-recycling/#:~:text=Recycling%20the%20waste%20from%20electronics,landfills%20in%20the%20first%20place.&text=Goods%20made%20from%20recycled%20materials,pollution%2C%20and%20uses%20less%20energy.",
-  // },
-  // {
-  //   category: "Electronics",
-  //   facts: "E-waste is hazardous material. Over time, electronics can leak toxic elements, like mercury and lead, which can be harmful to the environment and to humans. Donating your electronics for reuse or recycling them at safely managed sites helps control the hazards.",
-  //   source:
-  //     "https://www.cbc.ca/news/science/what-happens-to-the-e-waste-you-drop-off-for-recycling-1.5101357",
-  // },
-  // {
-  //   category: "Clothes",
-  //   facts: "Every pound of clothing contributes about 3-4 pounds of CO2 and other greenhouse gasses into the atmosphere.",
-  //   source:
-  //     "https://greenschoolsnationalnetwork.org/how-does-recycling-your-clothes-and-shoes-help-protect-the-earth/",
-  // },
-  // {
-  //   category: "Food",
-  //   facts: "America wastes roughly 40 percent of its food. Of the estimated 125 to 160 billion pounds of food that goes to waste every year, much of it is perfectly edible and nutritious.",
-  //   source: "https://foodprint.org/issues/the-problem-of-food-waste/",
-  // },
-  // {
-  //   category: "Shoes",
-  //   facts: "Improper disposal of shoes contributes to approximately 300-400 million pounds of CO2 entering the atmosphere each year, which is the equivalent of 26,000-35,000 cars onthe road.",
-  //   source:
-  //     "https://greenschoolsnationalnetwork.org/how-does-recycling-your-clothes-and-shoes-help-protect-the-earth/",
-  // },
-  // {
-  //   category: "Toys",
-  //   facts: "80 percent of toys end up in a landfill, incinerators, or the ocean. 90 percent are made of plastic and are used for just six months on average.",
-  //   source:
-  //     "https://fashnerd.com/2018/05/ecobirdy-sustainable-recycling-innovative-technology/#:~:text=According%20to%20ecoBirdy's%20website%2C%2080,just%20six%20months%20on%20average.",
-  // },
-  // {
-  //   category: "Books",
-  //   facts: "Approximately 640,000 tons of books are discarded into the landfill.",
-  //   source:
-  //     "https://www.nwf.org/~/media/PDFs/Eco-schools/McGraw%20Hill/12-4-12%20A%20Research%20Study%20on%20Textbook%20Recycling.ashx",
-  // },
-  // {
-  //   category: "Hardware",
-  //   facts: "Recycling the waste from electronics saves space in the landfills and prevents the environmental pollution caused by the toxins.",
-  // },
+  {
+    category: "Electronics",
+    fact: "E-waste is hazardous material. Over time, electronics can leak toxic elements, like mercury and lead, which can be harmful to the environment and to humans. Donating your electronics for reuse or recycling them at safely managed sites helps control the hazards.",
+    source:
+      "https://www.cbc.ca/news/science/what-happens-to-the-e-waste-you-drop-off-for-recycling-1.5101357",
+  },
+  {
+    category: "Clothes",
+    fact: "Every pound of clothing contributes about 3-4 pounds of CO2 and other greenhouse gasses into the atmosphere.",
+    source:
+      "https://greenschoolsnationalnetwork.org/how-does-recycling-your-clothes-and-shoes-help-protect-the-earth/",
+  },
+  {
+    category: "Food",
+    fact: "America wastes roughly 40 percent of its food. Of the estimated 125 to 160 billion pounds of food that goes to waste every year, much of it is perfectly edible and nutritious.",
+    source: "https://foodprint.org/issues/the-problem-of-food-waste/",
+  },
+  {
+    category: "Shoes",
+    fact: "Improper disposal of shoes contributes to approximately 300-400 million pounds of CO2 entering the atmosphere each year, which is the equivalent of 26,000-35,000 cars onthe road.",
+    source:
+      "https://greenschoolsnationalnetwork.org/how-does-recycling-your-clothes-and-shoes-help-protect-the-earth/",
+  },
+  {
+    category: "Toys",
+    fact: "80 percent of toys end up in a landfill, incinerators, or the ocean. 90 percent are made of plastic and are used for just six months on average.",
+    source:
+      "https://fashnerd.com/2018/05/ecobirdy-sustainable-recycling-innovative-technology/#:~:text=According%20to%20ecoBirdy's%20website%2C%2080,just%20six%20months%20on%20average.",
+  },
+  {
+    category: "Books",
+    fact: "Approximately 640,000 tons of books are discarded into the landfill.",
+    source:
+      "https://www.nwf.org/~/media/PDFs/Eco-schools/McGraw%20Hill/12-4-12%20A%20Research%20Study%20on%20Textbook%20Recycling.ashx",
+  },
+  {
+    category: "Hardware",
+    fact: "Recycling the waste from electronics saves space in the landfills and prevents the environmental pollution caused by the toxins.",
+  },
   {
     category: "Kitchenware",
-        facts: ["Some cookware is made of ferrous metal. One ton of ferrous scrap metal is about 2500 pounds of virgin ore, 1400 pounds of coal, and 120 pounds of limestone.", "Most modern cookware items have a blend of plastic and metal, and special chemical coatings that can't be recycled and must be removed."],
+    fact: "Pans and pots have a special coating which must be removed.",
+  source: "https://cookwarecrazy.com/post/how_to_throw_out_pots_and_pans_nyc"},
+  {
+    category: "Furniture",
+    fact: "According to the Environmental Protection Agency, the amount of furniture and furnishings taken to a landfill increases every year. Over 9.69 million tons goes to landfills.",
     source:
-      "https://www.marthastewart.com/1539762/how-to-recycle-old-pots-pans-cookware",
-    source2:
-      "https://cookwarecrazy.com/post/how_to_throw_out_pots_and_pans_nyc",
+      "https://archive.curbed.com/2019/4/11/18303532/furniture-waste-green-standards-landfill-recycling",
   },
-  // {
-  //   category: "Kitchenware",
-  //   fact: "Some cookware is made of ferrous metal. One ton of ferrous scrap metal is about 2500 pounds of virgin ore, 1400 pounds of coal, and 120 pounds of limestone.",
-  //   source:
-  //     "https://earth911.com/home-garden/how-to-recycle-your-old-cookware/",
-  //   source2: "https://www.fptscrap.com/ferrous-scrap-recycling-has-global-economic-and-environmental-effects/",
-  // },
-  // {
-  //   category: "Furniture",
-  //   facts: "According to the Environmental Protection Agency, the amount of furniture and furnishings taken to a landfill increases every year. Over 9.69 million tons goes to landfills.",
-  //   source:
-  //     "https://archive.curbed.com/2019/4/11/18303532/furniture-waste-green-standards-landfill-recycling",
-  // },
-  // {
-  //   category: "Jewelry",
-  //   facts: "Hazardous chemicals used in jewelry manufacturing pose a major environmental concern. These chemicals include cyanides, acids, solvents, compressed gases, investment dusts, polishing com pounds, and solders.",
-  //   source:
-  //     "https://dtsc.ca.gov/jewelry-manufacturing-pollution-prevention-recommendations-fact-sheet/",
-  // },
-  // {
-  //   category: "Arts and Crafts",
-  //   facts: "Certain art supplies have been a source of peril for artists. Cadmium, arsenic, lead, polyester resin, fiberglass, formaldehyde, and large pieces of metal are among the toxic materials that can be found in art supplies that adversely affect the environment.",
-  //   source:
-  //     "https://news.artnet.com/art-world/7-deadly-art-materials-to-watch-out-for-1081526",
-  // },
-  // {
-  //   category: "Sports and Outdoors",
-  //   facts: "Throwing away textiles, which can include sporting equipment, means those items will end up in the landfill. Since 2003, the percentage of textiles in landfills has increased 38 percent.",
-  //   source:
-  //     "https://www.wpr.org/expert-throwing-away-clothing-does-serious-damage-environment",
-  // },
-  // {
-  //   category: "Beauty and Health",
-  //   facts: "In the U.S. alone, over 7.9 billion units of rigid plastic is created for beauty and personal care products.",
-  //   source: "https://www.allure.com/story/beauty-industry-packaging-waste",
-  // },
-  // {
-  //   category: "Other",
-  //   facts: "Recycling and/or repurposing reduces the need for landfills.",
-  // },
+  {
+    category: "Jewelry",
+    fact: "Hazardous chemicals used in jewelry manufacturing pose a major environmental concern. These chemicals include cyanides, acids, solvents, compressed gases, investment dusts, polishing com pounds, and solders.",
+    source:
+      "https://dtsc.ca.gov/jewelry-manufacturing-pollution-prevention-recommendations-fact-sheet/",
+  },
+  {
+    category: "Arts and Crafts",
+    fact: "Certain art supplies have been a source of peril for artists. Cadmium, arsenic, lead, polyester resin, fiberglass, formaldehyde, and large pieces of metal are among the toxic materials that can be found in art supplies that adversely affect the environment.",
+    source:
+      "https://news.artnet.com/art-world/7-deadly-art-materials-to-watch-out-for-1081526",
+  },
+  {
+    category: "Sports and Outdoors",
+    fact: "Throwing away textiles, which can include sporting equipment, means those items will end up in the landfill. Since 2003, the percentage of textiles in landfills has increased 38 percent.",
+    source:
+      "https://www.wpr.org/expert-throwing-away-clothing-does-serious-damage-environment",
+  },
+  {
+    category: "Beauty and Health",
+    fact: "In the U.S. alone, over 7.9 billion units of rigid plastic is created for beauty and personal care products.",
+    source: "https://www.allure.com/story/beauty-industry-packaging-waste",
+  },
+  {
+    category: "Other",
+    fact: "Recycling and/or repurposing reduces the need for landfills.",
+  },
 ];
 
-// ["Some cookware is made of ferrous metal. One ton of ferrous scrap metal is about 2500 pounds of virgin ore, 1400 pounds of coal, and 120 pounds of limestone.", "Most modern cookware items have a blend of plastic and metal, and special chemical coatings that can't be recycled and must be removed."]
 module.exports = facts;
